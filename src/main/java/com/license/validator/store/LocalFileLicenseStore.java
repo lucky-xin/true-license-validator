4d0c281f6785a5ac1648109b2123654c5fcf69a7
luchaoxin <chaoxin.lu@pistonint.com>
2023-11-09 11:09:36 +0800

    修改包名称

---
+++ src/main/java/com/license/validator/store/LocalFileLicenseStore.java
@@ -0,0 +1,38 @@
+package com.license.validator.store;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import com.license.validator.entity.LicenseToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * License 校验成功缓存
+ *
+ * @author luchaoxin
+ * @version V 1.0
+ * @since 2023-11-08
+ */
+public class LocalFileLicenseStore implements LicenseStore {
+    private static final String LOCK_FILE_NAME = "lock";
+
+    @Override
+    public LicenseToken getLicenseToken() throws IOException {
+        try (InputStream lic = Files.newInputStream(Path.of(LOCK_FILE_NAME))) {
+            return JSON.parseObject(lic.readAllBytes(), new TypeReference<LicenseToken>() {
+            }.getType());
+        }
+    }
+
+    @Override
+    public void storeLicenseToken(LicenseToken token) throws IOException {
+        try (OutputStream out = Files.newOutputStream(Path.of(LOCK_FILE_NAME))) {
+            JSON.writeTo(out, token);
+        }
+    }
+}
+
