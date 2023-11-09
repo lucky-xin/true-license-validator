0214a22195838a3fc8e6b110b4b4dd52fd7c6134
luchaoxin <chaoxin.lu@pistonint.com>
2023-11-09 17:44:02 +0800

    支持System类型

--- src/main/java/com/license/validator/entity/CreatorParam.java
+++ src/main/java/com/license/validator/entity/CreatorParam.java
@@ -43,7 +43,7 @@ public class CreatorParam implements Serializable {
     /**
      * 用户类型: User:个人只能激活一次，System:企业，非必转
      */
-    private String consumerType = "User";
+    private String consumerType = "System";
 
     /**
      * 能激活次数，限制该证书能让几个客户使用，默认1，非必转,如果是User类型，则只能为1
